e945479f2adc389ed03d7be4f5190455aea27cc5
ariful97768 <ariful97768@gmail.com>
2024-12-07 19:22:59 +0600

    completed function to delete  data from database

--- index.js
+++ index.js
@@ -58,6 +58,12 @@ async function run() {
       res.send(result)
     })
 
+    app.delete('/my_visa/delete/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await visaCollection.deleteOne(query)
+      res.send(result)
+    })
 
 
     // Send a ping to confirm a successful connection
