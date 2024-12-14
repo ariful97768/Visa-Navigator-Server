b0379371bee062657cfa9ba16e8895f266e06148
ariful97768 <ariful97768@gmail.com>
2024-12-14 14:24:36 +0600

    Solved "server not working" issue.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+require('dotenv').config()
 const express = require('express');
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
-require('dotenv').config()
 
 // middleware
 app.use(cors())
@@ -12,7 +12,8 @@ app.use(express.json())
 // console.log(process.env.DB_user, process.env.DB_pass)
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@cluster0.wwjbp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// "mongodb+srv://<db_username>:<db_password>@cluster0.wwjbp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wwjbp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -132,9 +133,9 @@ run().catch(console.dir);
 
 
 
-// app.get('/', (req, res) => {
-//   res.send("Visa  server is running")
-// })
+app.get('/', (req, res) => {
+  res.send("Visa  server is running")
+})
 app.listen(port, () => {
   console.log('Visa server is running on port:', port)
 })
